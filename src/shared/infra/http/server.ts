406b5ac102eb96d9aef889def39531f432dd85a8
Jonathan Matheus <jonathanmatheus.1997.jm@gmail.com>
2020-08-15 17:12:44 -0300

    Refatorando aplicação para deixar camdas mais separadas.

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import app from './app';
 
-import './database';
+import '@shared/infra/typeorm';
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
