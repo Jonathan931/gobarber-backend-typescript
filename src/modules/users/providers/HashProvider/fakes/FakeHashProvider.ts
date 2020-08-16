96e016563e7b96513e46e010402654b0d502a11f
Jonathan Matheus <jonathanmatheus.1997.jm@gmail.com>
2020-08-16 17:27:27 -0300

    Criando teste de user , appointment, e adicionaodo coverage

---
+++ src/modules/users/providers/HashProvider/fakes/FakeHashProvider.ts
@@ -0,0 +1,13 @@
+import IHashProdiver from '../models/IHashProvider';
+
+class FakeHashProvider implements IHashProdiver {
+  public async generateHash(payload: string): Promise<string> {
+    return payload;
+  }
+
+  public async compareHash(payload: string, hashed: string): Promise<boolean> {
+    return payload === hashed;
+  }
+}
+
+export default FakeHashProvider;
--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,9 +1,9 @@
-import { hash } from 'bcryptjs';
 import { inject, injectable } from 'tsyringe';
 
 import User from '@modules/users/infra/typeorm/entities/User';
-
 import AppError from '@shared/errors/AppError';
+
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 import IUserRepository from '../repositories/IUsersRepository';
 
 interface IRequest {
@@ -17,6 +17,8 @@ class CreateUserService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUserRepository,
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
   ) {}
 
   public async execute({ name, email, password }: IRequest): Promise<User> {
@@ -26,7 +28,7 @@ class CreateUserService {
       throw new AppError('Email address already used.');
     }
 
-    const hashedPassword = await hash(password, 8);
+    const hashedPassword = await this.hashProvider.generateHash(password);
 
     const user = await this.usersRepository.create({
       name,
